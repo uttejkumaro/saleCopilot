5933d8f4b344d3937696cff1a795675d893002bb
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-03-07 15:31:28 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -54,7 +54,7 @@ def analyze_conversation_and_predict(conversation_history):
 
 # Streamlit Chatbot UI
 def main():
-    st.title("💼 B2B Chatbot using Gemini")
+    st.title("💼 SYPI")
 
     # Set up session state to manage conversation
     if "conversation_history" not in st.session_state:
